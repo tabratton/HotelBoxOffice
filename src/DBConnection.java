6d553525c5a3d7312277f51001ec4fae7ac1f5fd
metaspy <chad.goodwin@gmail.com>
2016-10-17 11:04:59 -0400

    Add files via upload

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBConnection {
@@ -9,7 +11,7 @@ public class DBConnection {
 	private String password = "masterRoot";
 	private Connection con;
 	
-	public DBConnection(String[] args) {
+	public DBConnection() {
 		try {
 		  con = DriverManager.getConnection(host, username, password);
 		} catch (SQLException ex) {
@@ -21,6 +23,21 @@ public class DBConnection {
 	
 	public Connection getCon(){
 		return con;
+	}
+	
+	public ResultSet searchStatement(String table, String field,
+			String value) {
+		PreparedStatement stm;
+		try{
+			stm = con.prepareStatement("SELECT * FROM ? WHERE ? LIKE ?");
+			stm.setString(1, table);
+			stm.setString(2, field);
+			stm.setString(3, value);
+			ResultSet resultSet = stm.executeQuery();
+			return resultSet;
+		} catch (SQLException e){
+			return null;
+		}
 	}
 	
 }
