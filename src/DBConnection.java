3f454697a6dc80df64b4f0074c63d6e4733e50fa
Tyler Bratton <tylerbratton96@gmail.com>
2016-10-17 19:19:47 -0400

    Add logic to get movie titles and images from database
    Also created a new function in DBConnection that allows searching of one entire table.

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,3 +1,4 @@
+import javax.xml.transform.Result;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -5,39 +6,50 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBConnection {
-	
-	private String host = "jdbc:mysql://hotelboxoffice.cornjso48dgu.us-east-1.rds.amazonaws.com:3306/HotelBoxOffice";
-	private String username = "masterRoot";
-	private String password = "masterRoot";
-	private Connection con;
-	
-	public DBConnection() {
-		try {
-		  con = DriverManager.getConnection(host, username, password);
-		} catch (SQLException ex) {
-		  System.out.println(ex.getMessage());
-		}
 
-		System.out.println("Connected");
-	}
-	
-	public Connection getCon(){
-		return con;
-	}
-	
-	public ResultSet searchStatement(String table, String field,
-			String value) {
-		PreparedStatement stm;
-		try{
-			stm = con.prepareStatement("SELECT * FROM ? WHERE ? LIKE %?%");
-			stm.setString(1, table);
-			stm.setString(2, field);
-			stm.setString(3, value);
-			ResultSet resultSet = stm.executeQuery();
-			return resultSet;
-		} catch (SQLException e){
-			return null;
-		}
-	}
-	
+  private String HOST = "jdbc:mysql://hotelboxoffice.cornjso48dgu" +
+      ".us-east-1.rds.amazonaws.com:3306/HotelBoxOffice";
+  private String USER_NAME = "masterRoot";
+  private String PASS_WORD = "masterRoot";
+  private Connection con;
+
+  public DBConnection() {
+    try {
+      con = DriverManager.getConnection(HOST, USER_NAME, PASS_WORD);
+      System.out.println("Connected");
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+    }
+  }
+
+  public Connection getCon() {
+    return con;
+  }
+
+  public ResultSet searchStatement(String table) {
+    PreparedStatement stm;
+    try {
+      String s = String.format("SELECT *FROM %s", table);
+      stm = con.prepareStatement(s);
+      return stm.executeQuery();
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+      return null;
+    }
+  }
+
+  public ResultSet searchStatement(String table, String field, String value) {
+    PreparedStatement stm;
+    try {
+      stm = con.prepareStatement("SELECT * FROM ? WHERE ? LIKE ?");
+      stm.setString(1, table);
+      stm.setString(2, field);
+      stm.setString(3, value);
+      return stm.executeQuery();
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+      return null;
+    }
+  }
+
 }
--- src/MovieGridController.java
+++ src/MovieGridController.java
@@ -1,55 +1,74 @@
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.event.ActionEvent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.TextAlignment;
 
+import javax.imageio.ImageIO;
 import java.awt.event.ActionListener;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-// TODO: Make logic related to database, need database first.
-
 public class MovieGridController implements Initializable {
   @FXML
-  private GridPane pane;
+  private FlowPane pane;
   public static String lastClickedMovie;
-
-  public void selectMovie(ActionEvent event) {
-    Button button = (Button) event.getSource();
-
-    lastClickedMovie = button.getText();
-
-  }
 
   public void initialize(URL url, ResourceBundle bundle) {
     ArrayList<Button> buttons = new ArrayList<>();
-    for (int i = 0; i < 15; i++) {
-      buttons.add(new Button(String.format("%d", i)));
-      buttons.get(i).setContentDisplay(ContentDisplay.TOP);
-      buttons.get(i).setOnAction(new EventHandler<ActionEvent>() {
-        @Override public void handle(ActionEvent event) {
-          Button button = (Button) event.getSource();
-          lastClickedMovie = button.getText();
-        }
-      });
+    DBConnection con = new DBConnection();
+    ResultSet rs = con.searchStatement("MOVIES");
 
-      Image image = new Image("PLACEHOLDER.jpg");
+    try {
+      rs.last();
+      int numRows = rs.getRow();
+      rs.first();
 
-      buttons.get(i).setGraphic(new ImageView(image));
-    }
+      for (int i = 0; i < numRows; i++) {
+        buttons.add(new Button(String.format("%s", rs.getString("MOVIE_TITLE")
+        )));
+        Button currentButton = buttons.get(i);
+        currentButton.setStyle("-fx-background-color: transparent");
+        currentButton.setContentDisplay(ContentDisplay.TOP);
+        currentButton.setPrefSize(150, 200);
+        currentButton.setWrapText(true);
+        currentButton.setAlignment(Pos.TOP_CENTER);
+        currentButton.setTextAlignment(TextAlignment.CENTER);
+        currentButton.setOnAction(new EventHandler<ActionEvent>() {
+          @Override
+          public void handle(ActionEvent event) {
+            Button button = (Button) event.getSource();
+            System.out.println(button.getText());
+            lastClickedMovie = button.getText();
+            //HotBoxNavigator.loadPage(HotBoxNavigator.MOVIE_PAGE);
+          }
+        });
 
-    int counter = 0;
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 5; j++, counter++) {
-        pane.add(buttons.get(counter), j, i);
+        ImageView imageView = new ImageView(rs.getString("MOVIE_IMAGE"));
+        currentButton.setGraphic(imageView);
+
+        pane.getChildren().add(currentButton);
+        rs.next();
       }
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
     }
+
   }
 }
