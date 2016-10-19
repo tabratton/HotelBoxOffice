9c714b7f6fc5e981246033f6578858702e60dd79
Tyler Bratton <tylerbratton96@gmail.com>
2016-10-18 20:45:37 -0400

    Reformat to adhere to Google Style

--- src/CustomerEditController.java
+++ src/CustomerEditController.java
@@ -1,75 +1,75 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
-
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 /**
- * FXML Controller class
+ * FXML Controller class.
  *
  * @author chad
  */
 
 public class CustomerEditController implements Initializable {
-    
-    /**
-     * Initializes the controller class.
-     */
-    @FXML // initialize all named objects in fxml
-    private TextField cID;
-    private TextField cName;
-    private TextField cRoom;
-    private TextField cBalance;
-    private TextField cPassword;
-    private TextField cConfirm;
-    private Button submitButton;
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
-        DBConnection con = new DBConnection();
-        String value = "1";
-        if (value == "2"){
-            ResultSet rs = con.searchStatement("CUSTOMER", "CUSTOMER_ID", value);
-            try {
-                while (rs.next()){
-                    cID.setText(rs.getString("CUSTOMER_ID"));
-                    cName.setText(rs.getString("CUSTOMER_NAME"));
-                    cBalance.setText(rs.getString("CUSTOMER_BALANCE"));
-                    cRoom.setText(rs.getString("CUSTOMER_ROOMNUM"));
 
-                }
-            } catch (SQLException e){
-                // error handling
-            }
-        }
-        submitButton.setOnAction( 
+  /**
+   * Initializes the controller class.
+   */
+  @FXML // initialize all named objects in fxml
+  private TextField customerId;
+  @FXML
+  private TextField customerName;
+  @FXML
+  private TextField customerRoom;
+  @FXML
+  private TextField customerBalance;
+  @FXML
+  private TextField customerPassword;
+  @FXML
+  private TextField customerConfirm;
+  @FXML
+  private Button submitButton;
 
-            new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                String name = cName.getText();
-                String bal = cBalance.getText();
-                String room = cRoom.getText();
-                String ID = cID.getText();
-                String upString =String.format( "UPDATE CUSTOMER SET CUSTOMER_NAME=%s,"
-                        + " CUSTOMER_BALANCE=%s, CUSTOMER_ROOMNUM=%s"
-                        + "WHERE CUSTOMER_ID=%s", name, bal, room, ID);
-            }
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+
+    DatabaseConnection con = new DatabaseConnection();
+    String value = "1";
+    if (value.equals("2")) {
+      ResultSet rs = con.searchStatement("CUSTOMER", "CUSTOMER_ID", value);
+      try {
+        while (rs.next()) {
+          customerId.setText(rs.getString("CUSTOMER_ID"));
+          customerName.setText(rs.getString("CUSTOMER_NAME"));
+          customerBalance.setText(rs.getString("CUSTOMER_BALANCE"));
+          customerRoom.setText(rs.getString("CUSTOMER_ROOMNUM"));
+
         }
-        );
-    }    
-    
+      } catch (SQLException ex) {
+        // error handling
+      }
+    }
+    submitButton.setOnAction(
+
+        new EventHandler<ActionEvent>() {
+          public void handle(ActionEvent event) {
+            String name = customerName.getText();
+            String bal = customerBalance.getText();
+            String room = customerRoom.getText();
+            String id = customerId.getText();
+            String upString = String.format("UPDATE CUSTOMER SET"
+                + " CUSTOMER_NAME=%s, CUSTOMER_BALANCE=%s, CUSTOMER_ROOMNUM=%s"
+                + " WHERE CUSTOMER_ID=%s", name, bal, room, id);
+          }
+        }
+    );
+  }
+
 
 }
