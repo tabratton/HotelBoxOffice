412f3e605c4789908bb28d79e89959537d940b3a
Tyler Bratton <tylerbratton96@gmail.com>
2016-10-20 14:21:17 -0400

    Fix ResultSet cursor so that results are saved to variables correctly

--- src/MoviePageController.java
+++ src/MoviePageController.java
@@ -1,12 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
@@ -16,49 +9,45 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.ResourceBundle;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
 /**
- * FXML Controller class
+ * FXML Controller class.
  *
  * @author gabrielguillen
  */
-public class MoviePageController 
-        implements Initializable {
-    
-    @FXML
-    private ListView<String> listView;
-    
-    
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb)  {
-        // HashMap to store MOVIE_TITLE as a key and MOVIE_ID as a value
+public class MoviePageController implements Initializable {
+
+  @FXML
+  private ListView<String> listView;
+
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // HashMap to store MOVIE_TITLE as a key and MOVIE_ID as a value
     HashMap<String, String> titleKeys = new HashMap<String, String>();
     // Initialize database connection
     DatabaseConnection con = new DatabaseConnection();
     // Get the MOVIE_ID, MOVIE_TITLE, and MOVIE_IMAGE columns from the MOVIES
     // table
-    ResultSet rs = con.searchStatement("MOVIES","MOVIE_ID",
-            HotBoxNavigator.lastClickedMovie);
+    ResultSet rs = con.searchStatement("MOVIES", "MOVIE_ID",
+        HotBoxNavigator.lastClickedMovie);
 
-    
-        try{ 
-                String director = rs.getString("MOVIE_DIRECTOR");
-                String description = rs.getString("MOVIE_DESCRIPTION");
-                String releaseDate = rs.getString("MOVIE_RELEASE_DATE");
-            //Things for the ListView
-            ObservableList<String> data = FXCollections.observableArrayList(director
-                    , description, releaseDate);
-            listView.setItems(data);
-         }catch (SQLException ex) {
+    try {
+      rs.first();
+      String director = "Director: " + rs.getString("MOVIE_DIRECTOR");
+      String description = "Description: " + rs.getString("MOVIE_DESCRIPTION");
+      String releaseDate = "Release Date: " + rs.getString("MOVIE_"
+          + "RELEASE_DATE");
+      //Things for the ListView
+      ObservableList<String> data = FXCollections.observableArrayList(director,
+          description, releaseDate);
+      listView.setItems(data);
+    } catch (SQLException ex) {
       System.out.println(ex.getMessage());
     }
-    }    
-    
-    
-    
+  }
+
+
 }
