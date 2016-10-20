0145084c943149512f6bb49fbf875d8649f715c8
gaguillen4384 <gaguillen4384@eagle.fgcu.edu>
2016-10-20 17:07:48 -0400

    Add text to the label  and image to the button

--- src/MoviePageController.java
+++ src/MoviePageController.java
@@ -1,14 +1,10 @@
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-
 import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class.
@@ -20,6 +16,13 @@ public class MoviePageController implements Initializable {
   @FXML
   private ListView<String> listView;
 
+  @FXML
+  private Button playMovie;
+  
+  @FXML
+  private Label movieTitle;
+  
+  
   /**
    * Initializes the controller class.
    */
@@ -35,6 +38,7 @@ public class MoviePageController implements Initializable {
         HotBoxNavigator.lastClickedMovie);
 
     try {
+        //set variables with data from the database
       rs.first();
       final String title = rs.getString("MOVIE_TITLE");
       final String director = "Director: " + rs.getString("MOVIE_DIRECTOR");
@@ -43,7 +47,14 @@ public class MoviePageController implements Initializable {
       final String releaseDate = "Release Date: " + rs.getString("MOVIE_"
           + "RELEASE_DATE");
       final String movieImage = rs.getString("MOVIE_IMAGE");
-
+      
+      //sets title for the page
+      movieTitle.setText(title);
+      
+      //sets image for the play movie button
+      playMovie.setGraphic(ImageUtility.getImage(movieImage,420,500));
+      
+      
       //Things for the ListView
       ObservableList<String> data = FXCollections.observableArrayList(director,
           description, releaseDate);
@@ -52,6 +63,3 @@ public class MoviePageController implements Initializable {
       System.out.println(ex.getMessage());
     }
   }
-
-
-}
