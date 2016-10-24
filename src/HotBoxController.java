4082b3ddf278cd13ebb7346148eaf0d2607e5976
Tyler Bratton <tylerbratton96@gmail.com>
2016-10-24 16:34:19 -0400

    Add simple navigation bar with a search box.

--- src/HotBoxController.java
+++ src/HotBoxController.java
@@ -1,6 +1,7 @@
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 
 /**
@@ -11,9 +12,10 @@ public class HotBoxController {
   /** Holder of a switchable view. */
   @FXML
   private StackPane hotBoxHolder;
-
   @FXML
   private Label headerLabel;
+  @FXML
+  private TextField searchBox;
 
   /**
    * Replaces the view displayed in the view holder with a new view.
@@ -24,4 +26,29 @@ public class HotBoxController {
     hotBoxHolder.getChildren().setAll(node);
   }
 
+  public void loadSearchResults() {
+    HotBoxNavigator.lastSearchTerm = searchBox.getText();
+    HotBoxNavigator.loadPage(HotBoxNavigator.SEARCH_RESULTS);
+  }
+
+  public void loadMovieGrid() {
+    resetSearchBox();
+    HotBoxNavigator.loadPage(HotBoxNavigator.MOVIE_GRID);
+  }
+
+  public void loadActorList() {
+    resetSearchBox();
+    HotBoxNavigator.loadPage(HotBoxNavigator.ACTOR_LIST);
+  }
+
+  public void loadAdminPage() {
+    resetSearchBox();
+    //HotBoxNavigator.loadPage(HotBoxNavigator.ADMIN_PAGE);
+    //For now just load customer edit since it's the only admin tool we have.
+    HotBoxNavigator.loadPage(HotBoxNavigator.CUSTOMER_EDIT);
+  }
+
+  private void resetSearchBox() {
+    searchBox.setText("");
+  }
 }
