a638d386dc7f3d9189dfb09800dc7d9acd382acd
Tyler Bratton <tylerbratton96@gmail.com>
2016-11-13 22:38:50 -0500

    Fix call to search statement to match changes to the search statement.

--- src/ActorPageController.java
+++ src/ActorPageController.java
@@ -19,7 +19,7 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
 /**
- * FXML Controller class
+ * FXML Controller class.
  *
  * @author Vilma Un Jan
  */
@@ -30,16 +30,16 @@ public class ActorPageController implements Initializable {
 
   @FXML
   private Button actorImageButton;
-  
+
   @FXML
   private Label actorName;
-  
+
   @FXML
   private Label actorBio;
-  
+
   @FXML
   private Label listMovieLabel;
-  
+
   @FXML
   private ListView<String> listView;
 
@@ -51,11 +51,11 @@ public class ActorPageController implements Initializable {
 
     ResultSet actorPage = HotelBox.dbConnection.searchStatement("ACTORS",
         "ACTOR_ID", HotBoxNavigator.lastClickedActor);
-    
-    
+
+
     ResultSet movieList = HotelBox.dbConnection.searchStatement("MOVIE_TITLE",
-            "MOVIES","CASTING", "ACTORS", "ACTOR_NAME",
-            HotBoxNavigator.lastClickedActor, "MOVIE_ID", "ACTOR_ID");
+        "MOVIES", "CASTING", "ACTORS", "ACTOR_ID",
+        HotBoxNavigator.lastClickedActor, "MOVIE_ID", "ACTOR_ID");
 
     try {
       actorPage.first();
@@ -63,49 +63,50 @@ public class ActorPageController implements Initializable {
       final String actorImage = actorPage.getString("ACTOR_IMAGE");
       final String bio = "Biography: " + actorPage.getString(
           "ACTOR_BIO");
-      final String movieLabel = actorPage.getString("ACTOR_NAME")+" movies:";
-      
-    /// final ArrayList<String> movies = new ArrayList<String>();
-    //  try {
-          movieList.last();
-          int numRows = movieList.getRow();
-          movieList.first();
-          
-          //Add movies to the list
-          for(int i = 0; i < numRows; i++) {
-              String movieName = movieList.getString("MOVIE_TITLE");
-            //  movies.add(movieName);
-              listView.getItems().add(movieName);
-              movieList.next();
-          }
-          
-    //  }catch (SQLException ex){
-    //      System.out.println(ex.getMessage());
-    //  }
-      
+      final String movieLabel = actorPage.getString("ACTOR_NAME") + " movies:";
+
+      /// final ArrayList<String> movies = new ArrayList<String>();
+      //  try {
+      movieList.last();
+      int numRows = movieList.getRow();
+      movieList.first();
+
+      //Add movies to the list
+      for (int i = 0; i < numRows; i++) {
+        String movieName = movieList.getString("MOVIE_TITLE");
+        //  movies.add(movieName);
+        listView.getItems().add(movieName);
+        movieList.next();
+      }
+
+      //  }catch (SQLException ex){
+      //      System.out.println(ex.getMessage());
+      //  }
+
       actorName.setWrapText(true);
       actorName.setTextAlignment(TextAlignment.CENTER);
       actorName.setFont(new Font(30.0));
       actorName.setText(name);
-      
+
       actorBio.setWrapText(true);
       actorBio.setTextAlignment(TextAlignment.CENTER);
       actorBio.setText(bio);
-      
+
       listMovieLabel.setWrapText(true);
       listMovieLabel.setTextAlignment(TextAlignment.CENTER);
       listMovieLabel.setText(movieLabel);
-           
-      actorImageButton.setGraphic(GeneralUtilities.getImage(actorImage, 300, 450));
+
+      actorImageButton.setGraphic(GeneralUtilities.getImage(actorImage, 300,
+          450));
       actorImageButton.setStyle("-fx-background-color: transparent;");
-      
+
       goBack.setOnAction(new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent event) {
-        HotBoxNavigator.loadPage(HotBoxNavigator.lastPageLoaded);
+          HotBoxNavigator.loadPage(HotBoxNavigator.lastPageLoaded);
         }
       });
-            
+
     } catch (SQLException ex) {
       System.out.println(ex.getMessage());
     }
