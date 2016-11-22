78023b73ca83ea1336f833917a3fc663f9514e8f
Vilma Un Jan vilmaunjan@gmail.com <Vilma@172.26.211.86>
2016-11-22 15:42:21 -0500

    Changed search statement so that it searches for the movie image. Removed parameter that was repeated

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -146,31 +146,30 @@ public class DatabaseConnection {
     }
   }
 
+
   /**
    * USING THIS SEARCH TO GET THE LIST OF MOVIE FOR EACH ACTOR.
    * Automatically constructs search statement and executes it.
    *
-   * @param field1 The field that you want to display (MOVIE_TITLE) to find
-   *               movies or (ACTOR_NAME) to find actors.
    * @param table1 The table that has field1 (MOVIES).
    * @param table2 The table that has info from table1 and table3 (CASTING).
    * @param table3 The table that has the field2 used to look for field1 (ACTORS).
-   * @param field2 The field from table3 (ACTOR_ID) when finding movies or
-   *               (MOVIE_ID) when finding actors.
+   * @param field1 The field from table1 (MOVIE_ID)
+   * @param field2 The field from table3 (ACTOR_ID)
+   *               
    * @param value  The pattern that you are looking for
-   * @param field3 The field that is in table1 and table2 (MOVIE_ID).
-   * @param field4 The field that is in table2 and table3 (ACTOR_ID).
    * @return A ResultSet object that contains the information you requested.
    */
-  public ResultSet searchStatement(String field1, String table1, String table2,
-                                   String table3, String field2, String value,
-                                   String field3, String field4) {
+   
+   public ResultSet searchStatement (String table1, String table2, String table3,
+                                     String value, String field1, String field2){
+       
     PreparedStatement stm;
     try {
-      String search = String.format("SELECT %s FROM %s, %s, %s WHERE %s.%s ="
-              + " %s AND %s.%s = %s.%s AND %s.%s = %s.%s", field1, table1,
-          table2, table3, table2, field2, value, table2, field3, table1, field3,
-          table2, field4, table3, field4);
+      String search = String.format("SELECT * FROM %s, %s, %s WHERE %s.%s ="
+              + " %s AND %s.%s = %s.%s AND %s.%s = %s.%s", table1,
+          table2, table3, table2, field2, value, table2, field1, table1, field1,
+          table2, field2, table3, field2);
       stm = con.prepareStatement(search);
       return stm.executeQuery();
     } catch (SQLException ex) {
