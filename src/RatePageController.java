1788c9d8063410b315ac192e9f09daf78be90659
gaguillen4384 <gaguillen4384@eagle.fgcu.edu>
2016-11-22 19:42:15 -0500

    Add skeleton of RatePageController
    I am really not  understanding how to update the database. get confuse using strings and integers.

---
+++ src/RatePageController.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+/**
+ * FXML Controller class
+ *
+ * @author gabrielguillen
+ */
+public class RatePageController implements Initializable {
+    
+    @FXML
+    private RadioButton one;
+    
+    @FXML
+    private RadioButton two;
+    
+    @FXML
+    private RadioButton three;
+    
+    @FXML
+    private RadioButton four;
+    
+    @FXML
+    private Button submit;
+    
+    
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    }    
+    
+}
