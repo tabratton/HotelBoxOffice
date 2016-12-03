51cd8e764ceaf7fa4e7c3e39d1f2f2a7d6a3817d
metaspy <chad.goodwin@gmail.com>
2016-12-02 23:04:45 -0500

    Add files via upload

---
+++ HotBoxNavigator.java
@@ -0,0 +1,98 @@
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+import java.util.Stack;
+
+/**
+ * Utility class for controlling navigation between views.
+ *
+ * <p>All methods on the navigator are static to facilitate
+ * simple access from anywhere in the application.
+ *
+ * @author Tyler Bratton
+ */
+public class HotBoxNavigator {
+
+  /**
+   * Convenience constants for fxml layouts managed by the navigator.
+   */
+  public static final String MAIN = "HotBox.fxml";
+  public static final String MOVIE_GRID = "MovieGrid.fxml";
+  public static final String MOVIE_PAGE = "MoviePage.fxml";
+  public static final String CUSTOMER_EDIT = "CustomerEdit.fxml";
+  public static final String SEARCH_RESULTS = "SearchResults.fxml";
+  public static final String ACTOR_PAGE = "ActorPage.fxml";
+  public static final String ACTOR_LIST = "ActorList.fxml";
+  public static final String ADMIN_PAGE = "AdminPage.fxml";
+  public static final String RATE_PAGE = "RatePage.fxml";
+  public static final String STATS_PAGE = "StatsPage.fxml";
+  public static final String LOGIN_PAGE = "LoginPage.fxml";
+  public static final String EDIT_PAGE = "EditPage.fxml";
+  public static final String GENRE_EDIT = "GenreEdit.fxml";
+  public static final String MOVIE_EDIT = "MovieEdit.fxml";
+
+
+  /**
+   * Convenience static fields to determine the last clicked buttons.
+   */
+  //public static String lastClickedMovie;
+  //public static String lastClickedActor;
+  //public static String lastPageLoaded;
+  public static String lastSearchTerm;
+  public static Stack<String> lastLoadedPageStack = new Stack<>();
+  public static Stack<String> lastClickedMovieStack = new Stack<>();
+  public static Stack<String> lastClickedActorStack = new Stack<>();
+
+  /**
+   * Convenience static fields for Admin editing
+   */
+  public static String editTable = null;
+  public static String editRecord = null;
+  
+  /**
+   * The main application layout controller.
+   */
+  private static HotBoxController mainController;
+
+  /**
+   * Stores the main controller for later use in navigation tasks.
+   *
+   * @param mainController the main application layout controller.
+   */
+  public static void setMainController(HotBoxController mainController) {
+    HotBoxNavigator.mainController = mainController;
+  }
+
+  /**
+   * Loads the view specified by the fxml file into the
+   * hotBoxHolder pane of the main application layout.
+   *
+   * <p>Previously loaded view for the same fxml file are not cached.
+   * The fxml is loaded anew and a new view node hierarchy generated
+   * every time this method is invoked.
+   *
+   * <p>A more sophisticated load function could potentially add some
+   * enhancements or optimizations, for example:
+   * cache FXMLLoaders
+   * cache loaded view nodes, so they can be recalled or reused
+   * allow a user to specify view node reuse or new creation
+   * allow back and forward history like a browser
+   *
+   * @param fxml the fxml file to be loaded.
+   */
+  public static void loadPage(String fxml) {
+    try {
+      mainController.setPage(FXMLLoader.load(HotBoxNavigator.class
+          .getResource(fxml)));
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
+  }
+
+  public static void clearStacks() {
+    lastClickedActorStack.clear();
+    lastClickedMovieStack.clear();
+    lastLoadedPageStack.clear();
+  }
+
+}
---
+++ MovieEditController.java
@@ -0,0 +1,175 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author Chad
+ */
+public class MovieEditController implements Initializable {
+
+    @FXML
+    private TextField movieTitle;
+    @FXML
+    private TextField movieDirector;
+    @FXML
+    private TextArea movieDescription;
+    @FXML
+    private TextField movieDate;
+    @FXML
+    private TextField movieImage;
+    @FXML
+    private ChoiceBox<String> movieGenre;
+    @FXML
+    private TextField moviePrice;
+    @FXML
+    private TextField movieViewed;
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Button submitButton;
+    private String id;
+    private int numGenres=0;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        ResultSet genres = HotelBox.dbConnection.searchStatement("GENRE");
+        try{
+            ObservableList<String> options = FXCollections.observableArrayList();
+            while (genres.next()){
+                
+                String genreName = genres.getString("GENRE_NAME");
+                options.add(genreName);
+                numGenres++;
+            }
+            movieGenre.setItems(options);
+        }catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        if (HotBoxNavigator.editRecord!=null) {
+      
+            ResultSet rs = HotelBox.dbConnection.searchStatement("MOVIES",
+              "MOVIE_ID", HotBoxNavigator.editRecord);
+
+            try {
+                rs.first();
+                
+                id = rs.getString("MOVIE_ID");
+                movieTitle.setText(rs.getString("MOVIE_TITLE"));
+                movieDirector.setText(rs.getString("MOVIE_DIRECTOR"));
+                movieDescription.setText(rs.getString("MOVIE_DESCRIPTION"));
+                movieDate.setText(rs.getString("MOVIE_RELEASE_DATE"));
+                movieImage.setText(rs.getString("MOVIE_IMAGE"));
+                
+                Double balance = Double.parseDouble(rs.getString("MOVIE_PRICE"));
+                moviePrice.setText(String.format("%.2f", balance));
+                movieViewed.setText(Integer.toString(rs.getInt("TIMES_VIEWED")));
+                movieGenre.getSelectionModel().select(rs.getInt("GENRE_ID")-1);
+                
+                submitButton.setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event) {
+                 
+                    String title = movieTitle.getText();
+                    String director = movieTitle.getText();
+                    String description = movieDescription.getText();
+                    String date = movieDate.getText();
+                    String image = movieImage.getText();
+                    String price = moviePrice.getText();
+                    String viewed = movieViewed.getText();
+                    int genre=1;
+                    for (int i = 1; i<=numGenres; i++){
+                        if (movieGenre.getSelectionModel().isSelected(i)){
+                            genre = i+1;
+                        }
+                    }
+                    
+                    
+                    String upString = String.format("UPDATE MOVIES SET"
+                    + " MOVIE_TITLE='%s', MOVIE_DIRECTOR='%s', MOVIE_DESCRIPTION='%s',"
+                    + " MOVIE_RELEASE_DATE='%s', MOVIE_IMAGE='%s', MOVIE_PRICE='%s', TIMES_VIEWED=%s, GENRE_ID=%s"
+                    + " WHERE MOVIE_ID=%s", title, director, description, date, image, price, viewed, genre, id);
+                Connection con = HotelBox.dbConnection.getCon();
+
+                try {
+                    PreparedStatement stm;
+                    stm = con.prepareStatement(upString);
+                    stm.executeUpdate();
+                    HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+                }
+                });
+                
+                
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        
+        } else {
+            
+            String title = movieTitle.getText();
+            String director = movieTitle.getText();
+            String description = movieDescription.getText();
+            String date = movieDate.getText();
+            String image = movieImage.getText();
+            String price = moviePrice.getText();
+            String viewed = movieViewed.getText();
+            int genre=1;
+            for (int i = 1; i<=numGenres; i++){
+                if (movieGenre.getSelectionModel().isSelected(i)){
+                    genre = i+1;
+                }
+            }
+            String upString = String.format("INSERT INTO MOVIES VALUES"
+            + " (null,'%s','%s','%s','%s','%s',%s,%s,%s)", title, director, description, date, image, genre, price, viewed);
+                    Connection con = HotelBox.dbConnection.getCon();
+                    
+                    try {
+                        PreparedStatement stm;
+                        stm = con.prepareStatement(upString);
+                        stm.executeUpdate();
+                        
+                        
+                    } catch (SQLException ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                    HotBoxNavigator.loadPage(HotBoxNavigator.ADMIN_PAGE);
+        }
+        
+        
+        
+        
+        cancelButton.setOnAction( new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                HotBoxNavigator.loadPage(HotBoxNavigator.ADMIN_PAGE);
+            }
+        }
+        );
+    }    
+    
+}
