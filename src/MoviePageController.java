f3a0560a9d9f9e9862ab3bd3128d09008bb2b5b8
Tyler Bratton <tylerbratton96@gmail.com>
2016-12-03 20:10:57 -0500

    Formatting.

--- src/MoviePageController.java
+++ src/MoviePageController.java
@@ -19,7 +19,6 @@ import java.util.HashMap;
 import java.util.ResourceBundle;
 
 
-
 /**
  * FXML Controller class.
  *
@@ -42,8 +41,8 @@ public class MoviePageController implements Initializable {
 
   @FXML
   private Button goBackButton;
-  
-  @FXML 
+
+  @FXML
   private ListView<Button> actorList;
 
   private HashMap<String, String> keys = new HashMap<>();
@@ -58,15 +57,15 @@ public class MoviePageController implements Initializable {
     // Get the MOVIE_ID, MOVIE_TITLE, and MOVIE_IMAGE columns from the MOVIES
     // table
     String lastMovie = HotBoxNavigator.lastClickedMovieStack.peek();
-    
+
     ResultSet rs = HotelBox.dbConnection.searchStatement("MOVIES", "MOVIE_ID",
         lastMovie);
-    
+
     //search statement for listing casting for last clicked movie
     ResultSet movieList = HotelBox.dbConnection.searchStatement("SELECT * FROM"
-            + " MOVIES, CASTING, ACTORS WHERE CASTING.MOVIE_ID = '" + lastMovie
-            +"' AND CASTING.MOVIE_ID = MOVIES.MOVIE_ID AND "
-            + "CASTING.ACTOR_ID = ACTORS.ACTOR_ID", true);
+        + " MOVIES, CASTING, ACTORS WHERE CASTING.MOVIE_ID = '" + lastMovie
+        + "' AND CASTING.MOVIE_ID = MOVIES.MOVIE_ID AND "
+        + "CASTING.ACTOR_ID = ACTORS.ACTOR_ID", true);
     try {
       //set variables with data from the database
       rs.first();
@@ -93,9 +92,10 @@ public class MoviePageController implements Initializable {
         System.out.println(ex.getMessage());
       }
       //sets image for the play movie button
-      movieImageButton.setGraphic(GeneralUtilities.getImage(movieImage, 300, 450));
+      movieImageButton.setGraphic(GeneralUtilities.getImage(movieImage, 300,
+          450));
       movieImageButton.setStyle("-fx-background-color: transparent;");
-     
+
       // Sets the text that will be each item of the list, and sets the text
       // wrapping property so that the scroll bar is not needed.
       Text text = new Text(director);
@@ -107,19 +107,6 @@ public class MoviePageController implements Initializable {
       text = new Text(releaseDate);
       text.wrappingWidthProperty().bind(listView.widthProperty().subtract(30));
       listView.getItems().add(text);
-      
-      //For cast List
-//      movieList.last();
-//      int numRows = movieList.getRow();
-//
-//      movieList.first();
-//      actorList.getItems().add("Cast:");
-//      //add actors to the list
-//      for(int i=0; i<numRows; i++){
-//          String actorName = movieList.getString("ACTOR_NAME");
-//          actorList.getItems().add(actorName);
-//          movieList.next();
-//      }
 
       movieList.last();
       int numRows = movieList.getRow();
@@ -143,7 +130,8 @@ public class MoviePageController implements Initializable {
                 + ".TIMES_VIEWED = ACTORS.TIMES_VIEWED + 1 WHERE ACTORS"
                 + ".ACTOR_ID = " + currentId);
             //HotBoxNavigator.lastPageLoaded = currentPage;
-            HotBoxNavigator.lastLoadedPageStack.push(HotBoxNavigator.MOVIE_PAGE);
+            HotBoxNavigator.lastLoadedPageStack.push(HotBoxNavigator
+                .MOVIE_PAGE);
             // Once the movie page is made, this line will load it.
             // Ideally the initialize() method of that page will read
             // lastClickedMovie and use that string to load the correct data
@@ -155,18 +143,16 @@ public class MoviePageController implements Initializable {
         actorList.getItems().add(currentActor);
         movieList.next();
       }
-      
+
       goBackButton.setOnAction(new EventHandler<ActionEvent>() {
         @Override
         public void handle(ActionEvent event) {
           // Once the Button is clicked it goes back to the MainGrid
           //HotBoxNavigator.loadPage(HotBoxNavigator.lastPageLoaded);
           HotBoxNavigator.loadPage(HotBoxNavigator.lastLoadedPageStack.pop());
-
         }
-           
       });
-      
+
       //tried to set it to increase balance but failing 
       // is also trying to go RATE_Page failing too.
       playMovie.setOnAction(new EventHandler<ActionEvent>() {
@@ -175,24 +161,23 @@ public class MoviePageController implements Initializable {
           // Once pressed to rent a movie the balance of the movie gets
           // subtracted from the customer balance
           String upString = String.format("UPDATE CUSTOMER, MOVIES SET"
-              + " CUSTOMER.CUSTOMER_BALANCE = CUSTOMER.CUSTOMER_BALANCE +"
-              + " MOVIES.MOVIE_PRICE WHERE CUSTOMER.CUSTOMER_ID = %s AND"
-              + " MOVIES.MOVIE_ID = %s", HotelBox.getCurrentUserId(),
+                  + " CUSTOMER.CUSTOMER_BALANCE = CUSTOMER.CUSTOMER_BALANCE +"
+                  + " MOVIES.MOVIE_PRICE WHERE CUSTOMER.CUSTOMER_ID = %s AND"
+                  + " MOVIES.MOVIE_ID = %s", HotelBox.getCurrentUserId(),
               lastMovie);
           HotelBox.dbConnection.updateStatement(upString);
-          
+
           // insert new line in customer rentals
           DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
           Date date = new Date();
           upString = String.format("INSERT INTO CUSTOMER_RENTALS"
                   + " (CUSTOMER_ID, MOVIE_ID, RENTAL_DATE) VALUES (%s,%s,'%s')",
-                  HotelBox.getCurrentUserId(), lastMovie,dateFormat.format(
-                  date));
+              HotelBox.getCurrentUserId(), lastMovie, dateFormat.format(date));
           HotelBox.dbConnection.updateStatement(upString);
           HotBoxNavigator.loadPage(HotBoxNavigator.RATE_PAGE);
         }
       });
-      
+
       // Commented out and not removed in case we want to change back to this
       // code.
       //Things for the ListView
