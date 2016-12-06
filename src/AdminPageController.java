7ea74eae7a2a4c8b60ea2913e6004700189925ae
Tyler Bratton <tylerbratton96@gmail.com>
2016-12-05 19:58:59 -0500

    Change how edit casting works.

--- src/AdminPageController.java
+++ src/AdminPageController.java
@@ -1,16 +1,16 @@
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
- * FXML Controller class
+ * FXML Controller class.
  *
- * @author chad
+ * @author Chad Goodwin
  */
 public class AdminPageController implements Initializable {
 
@@ -31,16 +31,12 @@ public class AdminPageController implements Initializable {
   private Button editGenre;
   @FXML
   private Button statisticsButton;
-  
   @FXML
   private Button createCasting;
-  @FXML
-  private Button editCasting;
   @FXML
   private Button editActor;
   @FXML
   private Button createActor;
-  
 
   @Override
   public void initialize(URL url, ResourceBundle rb) {
@@ -53,103 +49,88 @@ public class AdminPageController implements Initializable {
         HotBoxNavigator.loadPage(HotBoxNavigator.STATS_PAGE);
       }
     });
-    
+
     // edit customer button
     editCustomer.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = "CUSTOMER";
-           HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
-           
-       }
+      @Override
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.editTable = "CUSTOMER";
+        HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
+      }
     });
-    // edit customer button
-    editCasting.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = "CASTING";
-           HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
-           
-       }
-    });
+
     // edit movie button
     editMovie.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = "MOVIES";
-           HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
-           
-       }
+      @Override
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.editTable = "MOVIES";
+        HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
+      }
     });
+
     // edit genre button
     editGenre.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = "GENRE";
-           HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
-           
-       }
+      @Override
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.editTable = "GENRE";
+        HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
+      }
     });
+
     // edit actor button
     editActor.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = "ACTORS";
-           HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
-           
-       }
+      @Override
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.editTable = "ACTORS";
+        HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
+      }
     });
-    
+
     // edit customer button
     createCustomer.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = null;
-           HotBoxNavigator.editRecord = null;
-           HotBoxNavigator.loadPage(HotBoxNavigator.CUSTOMER_EDIT);
-           
-       }
+      @Override
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.editTable = null;
+        HotBoxNavigator.editRecord = null;
+        HotBoxNavigator.loadPage(HotBoxNavigator.CUSTOMER_EDIT);
+      }
     });
-    
+
     // edit customer button
     createCasting.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = null;
-           HotBoxNavigator.editRecord = null;
-           HotBoxNavigator.loadPage(HotBoxNavigator.CASTING_EDIT);
-           
-       }
+      @Override
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.editTable = null;
+        HotBoxNavigator.editRecord = null;
+        HotBoxNavigator.loadPage(HotBoxNavigator.CASTING_EDIT);
+      }
     });
-    
+
     createGenre.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = null;
-           HotBoxNavigator.editRecord = null;
-           HotBoxNavigator.loadPage(HotBoxNavigator.GENRE_EDIT);
-           
-       }
+      @Override
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.editTable = null;
+        HotBoxNavigator.editRecord = null;
+        HotBoxNavigator.loadPage(HotBoxNavigator.GENRE_EDIT);
+      }
     });
-    
+
     createMovie.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = null;
-           HotBoxNavigator.editRecord = null;
-           HotBoxNavigator.loadPage(HotBoxNavigator.MOVIE_EDIT);
-           
-       }
+      @Override
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.editTable = null;
+        HotBoxNavigator.editRecord = null;
+        HotBoxNavigator.loadPage(HotBoxNavigator.MOVIE_EDIT);
+      }
     });
-    
+
     createActor.setOnAction(new EventHandler<ActionEvent>() {
-       @Override
-       public void handle(ActionEvent event) {
-           HotBoxNavigator.editTable = null;
-           HotBoxNavigator.editRecord = null;
-           HotBoxNavigator.loadPage(HotBoxNavigator.ACTOR_EDIT);
-           
-       }
+      @Override
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.editTable = null;
+        HotBoxNavigator.editRecord = null;
+        HotBoxNavigator.loadPage(HotBoxNavigator.ACTOR_EDIT);
+      }
     });
   }
 
--- src/CastingEditController.java
+++ src/CastingEditController.java
@@ -1,167 +1,134 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-import java.net.URL;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ResourceBundle;
+
 /**
- * FXML Controller class
+ * FXML Controller class.
  *
- * @author Chad
+ * @author Chad Goodwin
  */
 public class CastingEditController implements Initializable {
+  @FXML
+  private Button cancelButton;
+  @FXML
+  private Button submitButton;
+  @FXML
+  private Button deleteButton;
+  @FXML
+  private ComboBox<String> moviePicker;
+  @FXML
+  private ComboBox<String> actorPicker;
 
-    @FXML
-    private Button cancelButton;
-    @FXML
-    private Button submitButton;
-    @FXML
-    private ComboBox<String> moviePicker;
-    @FXML
-    private ComboBox<String> actorPicker;
+  private String id;
+  private Map actorName = new HashMap();
+  private Map movieTitle = new HashMap();
 
-    private int countMovies=0;
-    private int countActors = 0;
-    private String id;
-    private Map actorName= new HashMap();
-    private Map movieTitle= new HashMap();
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-       
-        
-        ResultSet movies = HotelBox.dbConnection.searchStatement("MOVIES");
-        try{
-            ObservableList<String> options = FXCollections.observableArrayList();
-            while (movies.next()){
-                
-                movieTitle.put(movies.getString("MOVIE_TITLE"), movies.getInt("MOVIE_ID"));
-                String movieName = movies.getString("MOVIE_TITLE");
-                options.add(movieName);
-                countMovies++;
-            }
-            moviePicker.setItems(options);
-        }catch (SQLException ex) {
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    ResultSet movies = HotelBox.dbConnection.searchStatement("MOVIES");
+    try {
+      ObservableList<String> options = FXCollections.observableArrayList();
+      while (movies.next()) {
+        movieTitle.put(movies.getString("MOVIE_TITLE"),
+            movies.getInt("MOVIE_ID"));
+        String movieName = movies.getString("MOVIE_TITLE");
+        options.add(movieName);
+      }
+      moviePicker.setItems(options);
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+    }
+
+    ResultSet actors = HotelBox.dbConnection.searchStatement("ACTORS");
+    try {
+      ObservableList<String> options = FXCollections.observableArrayList();
+      while (actors.next()) {
+        actorName.put(actors.getString("ACTOR_NAME"),
+            actors.getInt("ACTOR_ID"));
+        String actorName = actors.getString("ACTOR_NAME");
+        options.add(actorName);
+      }
+      actorPicker.setItems(options);
+    } catch (SQLException ex) {
+      System.out.println(ex.getMessage());
+    }
+
+    submitButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        int movie = (int) movieTitle.get(moviePicker.getValue());
+        int actor = (int) actorName.get(actorPicker.getValue());
+        ResultSet checkIfExists = HotelBox.dbConnection.searchStatement(
+            String.format("SELECT * FROM CASTING WHERE ACTOR_ID = %d AND"
+                + " MOVIE_ID = %d", actor, movie), true);
+        System.out.println(movie + " : " + actor);
+        try {
+          if (checkIfExists.next()) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Already Exists!");
+            alert.setHeaderText(null);
+            alert.setContentText("Casting entry already exists in database.");
+            alert.showAndWait();
+          } else {
+            String upString = String.format("INSERT INTO CASTING (ACTOR_ID,"
+                + " MOVIE_ID) VALUES (%s,%s)", actor, movie);
+            HotelBox.dbConnection.updateStatement(upString);
+            GeneralUtilities.showSuccessMessage();
+            HotBoxNavigator.loadPage(HotBoxNavigator.ADMIN_PAGE);
+          }
+        } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-        
-        ResultSet actors = HotelBox.dbConnection.searchStatement("ACTORS");
-        try{
-            ObservableList<String> options = FXCollections.observableArrayList();
-            while (actors.next()){
-                
-                actorName.put(actors.getString("ACTOR_NAME"), actors.getInt("ACTOR_ID") );
-                String actorName = actors.getString("ACTOR_NAME");
-                options.add(actorName);
-                countActors++;
-            }
-            actorPicker.setItems(options);
-        }catch (SQLException ex) {
-            System.out.println(ex.getMessage());
+      }
+    });
+
+    deleteButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        int movie = (int) movieTitle.get(moviePicker.getValue());
+        int actor = (int) actorName.get(actorPicker.getValue());
+        ResultSet checkIfExists = HotelBox.dbConnection.searchStatement(
+            String.format("SELECT * FROM CASTING WHERE ACTOR_ID = %d AND"
+                + " MOVIE_ID = %d", actor, movie), true);
+        System.out.println(movie + " : " + actor);
+        try {
+          if (checkIfExists.next()) {
+            String upString = String.format("DELETE FROM CASTING WHERE"
+                + " ACTOR_ID = %d AND MOVIE_ID = %d", actor, movie);
+            HotelBox.dbConnection.updateStatement(upString);
+            GeneralUtilities.showSuccessMessage();
+            HotBoxNavigator.loadPage(HotBoxNavigator.ADMIN_PAGE);
+          } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Does Not Exist!");
+            alert.setHeaderText(null);
+            alert.setContentText("Casting entry does not exist in database.");
+            alert.showAndWait();
+          }
+        } catch (SQLException ex) {
+          System.out.println(ex.getMessage());
         }
-        
-        
-        
-        if (HotBoxNavigator.editRecord!=null) {
-      System.out.println(HotBoxNavigator.editRecord);
-            ResultSet rs = HotelBox.dbConnection.searchStatement("CASTING",
-              "CASTING_ID", HotBoxNavigator.editRecord);
+      }
+    });
 
-            try {
-                rs.first();
-                
-                id = rs.getString("CASTING_ID");
-                
-                moviePicker.getSelectionModel().select(rs.getInt("MOVIE_ID")-1);
-                actorPicker.getSelectionModel().select(rs.getInt("ACTOR_ID")-1);
-                
-            submitButton.setOnAction(new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent event) {
-                 
-                    int movie = (int)movieTitle.get(moviePicker.getValue());
-                    
-                    int actor=(int)actorName.get(actorPicker.getValue());
+    cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+      public void handle(ActionEvent event) {
+        HotBoxNavigator.loadPage(HotBoxNavigator.ADMIN_PAGE);
+      }
+    });
+  }
 
-                    
-                    
-                    String upString = String.format("UPDATE CASTING SET"
-                    + "  MOVIE_ID=%s, ACTOR_ID=%s"
-                    + " WHERE CASTING_ID=%s", movie, actor, id);
-                Connection con = HotelBox.dbConnection.getCon();
-
-                try {
-                    PreparedStatement stm;
-                    stm = con.prepareStatement(upString);
-                    stm.executeUpdate();
-                    HotBoxNavigator.loadPage(HotBoxNavigator.EDIT_PAGE);
-                } catch (SQLException ex) {
-                    System.out.println(ex.getMessage());
-                }
-                }
-                });
-                
-                
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());
-            }
-        
-        } else {
-         
-            submitButton.setOnAction(new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent event) {
-                 
-                    
-                    int movie = (int)movieTitle.get(moviePicker.getValue());
-                    
-                    int actor=(int)actorName.get(actorPicker.getValue());
-                    
-                    System.out.println(movie + " : " + actor);
-                    String upString = String.format("INSERT INTO CASTING (ACTOR_ID, MOVIE_ID) VALUES (%s,%s)", actor , movie);
-                    Connection con = HotelBox.dbConnection.getCon();
-                    
-            
-                    
-                    try {
-                        PreparedStatement stm;
-                        stm = con.prepareStatement(upString);
-                        stm.executeUpdate();
-                    HotBoxNavigator.loadPage(HotBoxNavigator.ADMIN_PAGE);    
-                        
-                    } catch (SQLException ex) {
-                        System.out.println(ex.getMessage());
-                    }
-                }
-                });
-            
-                        
-        }
-        
-        
-        
-        cancelButton.setOnAction( new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                HotBoxNavigator.loadPage(HotBoxNavigator.ADMIN_PAGE);
-            }
-        });
-    }    
-    
 }
